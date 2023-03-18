2e9480baea4f37d6d05256136bd8c56104e87c16
RaphaelCMiranda <raphael.miranda@pravaler.com.br>
2023-03-18 12:54:14 -0300

    Add Testes unitarios

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '../use-cases/register'
+import { UserAlreadyExistsError } from '../../use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '../../use-cases/register'
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,9 +14,16 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({ name, email, password })
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({ name, email, password })
   } catch (err) {
-    return replay.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return replay.status(409).send({ message: err.message })
+    }
+
+    throw err
   }
 
   return replay.status(201).send()
